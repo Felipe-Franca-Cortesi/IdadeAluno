10077e9d66807224f3faf9d22eb27a36351866f1
Felipe França <contatofelipefranca12@gmail.com>
2020-10-07 15:37:34 -0300

    Adiciona a conclusão do exercício IdadeAluno

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IdadeAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Exemplo a ser seguido: dia/mês/ano \n");
+            Console.Write("Informe sua data de nascimento: ");
+            string dataNascimentoUsuario = Console.ReadLine();
+            DateTime dataNascimento = Convert.ToDateTime(dataNascimentoUsuario);
+            DateTime maiorIdade = dataNascimento.AddYears(18);
+            Console.WriteLine($"Seu nascimento foi, {dataNascimento.ToLongDateString()}");
+            Console.WriteLine($"Aniversário de 18 anos, {maiorIdade.ToLongDateString()}");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # IdadeAluno
-[console: IdadeAluno] Exiba a sua data de nascimento no formato de data longa. Mostre também a data do seu aniversário de 18 anos.
+*[console: IdadeAluno] Exiba a sua data de nascimento no formato de data longa. Mostre também a data do seu aniversário de 18 anos.*
+
+`Exemplo:`
+
+```
+Eu nasci em segunda-feira, 7 de setembro de 1981.
+Aniversário de 18 anos em terça-feira, 7 de setembro de 1999.
+```
